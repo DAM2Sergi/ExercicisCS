400117738eae4a475edd8a7a74ab1bccfa6c1120
Sergi Florensa <DAMpepito@outlook.com>
2022-10-10 19:34:29 +0200

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace ExercicisCS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Matrius2();
+        }
+
+
+        public static void Matrius2()
+        {
+
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            int num3 = Convert.ToInt32(Console.ReadLine());
+            int num4 = Convert.ToInt32(Console.ReadLine());
+            int num5 = Convert.ToInt32(Console.ReadLine());
+
+            int[] nums = { num1, num2, num3, num4, num5 };
+
+            for (int i=0; i<=nums.Length;i++)
+            {
+
+                Console.WriteLine(nums[i]);
+
+                nums[i + 1] = nums[i];
+
+            }
+
+
+
+        }
+
+        public static void Matrius3()
+        {
+
+            Console.WriteLine("Quants numeros vols introduir?");
+
+            int quantitat_num = Convert.ToInt32(Console.ReadLine());
+
+            int[] numeros = new int[quantitat_num];
+
+            for (int i = 0; i <= quantitat_num; i++)
+            {
+
+                numeros[i] = Convert.ToInt32(Console.ReadLine());
+
+            }
+
+        }
+
+    }
+}
