07707718d60e39e1a1d4154406d540fddfc88881
Sergi Florensa <DAMpepito@outlook.com>
2022-10-11 20:53:01 +0200

    Matrius 2 Fet

--- Program.cs
+++ Program.cs
@@ -18,18 +18,80 @@ namespace ExercicisCS
             int num3 = Convert.ToInt32(Console.ReadLine());
             int num4 = Convert.ToInt32(Console.ReadLine());
             int num5 = Convert.ToInt32(Console.ReadLine());
+            int costat;
 
             int[] nums = { num1, num2, num3, num4, num5 };
+            int[] array_desplaçat = new int[nums.Length];
 
-            for (int i=0; i<=nums.Length;i++)
+
+            do
             {
 
-                Console.WriteLine(nums[i]);
+                Console.WriteLine("Introdueix num depenent de el que vulguis fer");
+                Console.WriteLine("1.-Esquerra");
+                Console.WriteLine("2.-Dreta");
+                Console.WriteLine("3.-Sortir");
+                costat = Convert.ToInt32(Console.ReadLine());
 
-            }
+                if(costat == 1)
+                {
 
+                    for (int e=0;e<nums.Length;e++)
+                    {
 
+                        if(e == nums.Length-1)
+                        {
+                        
+                            array_desplaçat[4]= nums[0];
+                            Console.WriteLine(array_desplaçat[e]);
+                            continue;
+                            
+                        }
+ 
+                        array_desplaçat[e] = nums[e + 1];
+                        Console.WriteLine(array_desplaçat[e]);
 
+                    }
+
+                    for (int e = 0; e < nums.Length; e++)
+                    {
+                        
+                        nums[e] = array_desplaçat[e];
+
+                    }
+
+                }
+
+                if (costat == 2)
+                {
+
+                    for (int e = 0; e < nums.Length; e++)
+                    {
+
+                        if (e == nums.Length - 5)
+                        {
+
+                            array_desplaçat[0] = nums[4];
+                            Console.WriteLine(array_desplaçat[e]);
+                            continue;
+
+                        }
+
+                        array_desplaçat[e] = nums[e - 1];
+                        Console.WriteLine(array_desplaçat[e]);
+
+                    }
+
+                    for (int e = 0; e < nums.Length; e++)
+                    {
+
+                        nums[e] = array_desplaçat[e];
+
+                    }
+
+                }
+
+            } while (costat != 3);
         }
 
         public static void Matrius3()
@@ -46,6 +108,7 @@ namespace ExercicisCS
 
                 numeros[i] = Convert.ToInt32(Console.ReadLine());
 
+                suma +numeros[i];
             }
 
         }
