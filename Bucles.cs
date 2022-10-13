a4e269645a6cfcd7c1bcac7d8af2ed99747acf82
Sergi Florensa <DAMpepito@outlook.com>
2022-10-13 20:54:48 +0200

    Bucles 1 inici

--- Bucles.cs
+++ Bucles.cs
@@ -15,6 +15,23 @@ namespace ExercicisCS
         public static void Bucles1()
         {
 
+            int num1=Convert.ToInt32(Console.ReadLine());
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            int suma = 0;
+
+            for(int i = num1; i < num2; num1++)
+            {
+
+                suma ++;
+
+                Con
+
+            }
+
+
+
+
         }
     }
 
